49dd8152975640cbb855f2e950e2a1dd5dde7b85
Danny4life <osiki.segun@gmail.com>
2025-07-05 20:25:43 +0100

    edit file

---
+++ src/main/java/verlaClasses/oop/array/classActivity1/ArrayEx.java
@@ -0,0 +1,20 @@
+package verlaClasses.oop.array.classActivity1;
+
+public class ArrayEx {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        a[0]=10;//initialization
+        a[1]=20;
+        a[2]=70;
+        a[3]=40;
+        a[4]=50;
+        //traversing array
+        for(int i=0;i<a.length;i++){//length is the property of array
+            System.out.println(a[i]);
+        }
+
+
+        //int a[]={33,3,4,5};//declaration, instantiation and initialization
+
+    }
+}
---
+++ src/main/java/verlaClasses/oop/array/classActivity2/ArrayLoop.java
@@ -0,0 +1,14 @@
+package verlaClasses.oop.array.classActivity2;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+
+
+        int[] arr = {1,2,3,4,5};
+
+        for (int a : arr){
+            System.out.println(a);
+
+        }
+    }
+}
